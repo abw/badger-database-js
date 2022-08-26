845dd8b71b12c47bbd3244a4899ce4574d2c4832
Andy Wardley <abw@wardley.org>
2022-08-26 17:55:00 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export * from './Config.js';
 export * from './Connection.js';
+export * from './Database.js';
+export * from './Record.js';
 export * from './Schema.js';
+export * from './Table.js';
+export * from './Utils.js';
