47b0ede650a829ec4aea6c4e65ae5c5205321cc3
Andy Wardley <abw@wardley.org>
2022-08-29 17:30:09 +0100

    Test for database raw()

---
+++ test/database/raw.js
@@ -0,0 +1,21 @@
+import test from 'ava';
+import { database, createUsers } from '../library/users.js'
+
+test.before(
+  async t => {
+    await createUsers(database);
+    t.pass("created users table")
+  }
+);
+
+test.serial(
+  'database.query()',
+  async t => {
+    const rows = await database.raw('select forename from user where email="bobby@badger.com"');
+    t.is( rows[0].forename, 'Bobby' );
+  }
+)
+
+test.after(
+  () => database.destroy()
+)
