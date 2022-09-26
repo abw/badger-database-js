622bf4be4b453258e6aeadc793914a41f0963910
Andy Wardley <abw@wardley.org>
2022-09-26 18:55:35 +0100

    Deleted old drivers

--- src/Driver/Mysql/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Driver.js';
-export * from './Query.js';
-export { MysqlDriver as default } from './Driver.js'
--- src/Driver/Sqlite/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Driver.js';
-export * from './Query.js';
-export { SqliteDriver as default } from './Driver.js'
