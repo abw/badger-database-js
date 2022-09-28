c2164373757f8a1652a757cca55afcb38521b3ff
Andy Wardley <abw@wardley.org>
2022-09-28 15:56:59 +0100

    database tests

---
+++ test/database/database.js
@@ -0,0 +1,56 @@
+import test from 'ava';
+import { database } from '../../src/Database.js'
+
+let db;
+
+test.serial(
+  'database',
+  async t => {
+    db = await database({ engine: 'sqlite:memory' });
+    t.is( db.engine.driver, 'sqlite' );
+  }
+)
+
+test.serial(
+  'create',
+  async t => {
+    const create = await db.run(
+      `CREATE TABLE user (
+        id INTEGER PRIMARY KEY ASC,
+        name TEXT,
+        email TEXT
+      )`
+    );
+    t.is(create.changes, 0);
+  }
+)
+
+test.serial(
+  'insert a row',
+  async t => {
+    const insert = await db.run(
+      'INSERT INTO user (name, email) VALUES (?, ?)',
+      'Bobby Badger', 'bobby@badgerpower.com'
+    );
+    t.is(insert.changes, 1);
+  }
+)
+
+test.serial(
+  'fetch any row',
+  async t => {
+    const bobby = await db.any(
+      'SELECT * FROM user WHERE email=?',
+      'bobby@badgerpower.com'
+    );
+    t.is(bobby.name, 'Bobby Badger');
+  }
+)
+
+test.after(
+  'destroy',
+  async t => {
+    db.destroy();
+    t.pass();
+  }
+)
