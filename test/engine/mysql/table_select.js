3ec0472759335c6dedfb9761d755bb8eeb253d6f
Andy Wardley <abw@wardley.org>
2022-09-29 11:37:06 +0100

    Table select() tests

---
+++ test/engine/mysql/table_select.js
@@ -0,0 +1,96 @@
+import test from 'ava';
+import { database } from '../../../src/Database.js';
+import { engine } from '../../library/mysql.js';
+
+let db;
+
+test.serial(
+  'database',
+  async t => {
+    db = await database({
+      engine: engine,
+      tables: {
+        users: {
+          columns: 'id:readonly name:required email:required'
+        }
+      }
+    });
+    t.pass();
+  }
+)
+
+test.serial(
+  'drop table',
+  async t => {
+    await db.run(
+      `DROP TABLE IF EXISTS users`
+    )
+    t.pass();
+  }
+)
+
+test.serial(
+  'create table',
+  async t => {
+    await db.run(
+      `CREATE TABLE users (
+        id SERIAL,
+        name TEXT,
+        email TEXT
+      )`
+    )
+    t.pass();
+  }
+);
+
+test.serial(
+  'table insert',
+  async t => {
+    const users = await db.table('users');
+    const result = await users.insert({
+      name:  'Bobby Badger',
+      email: 'bobby@badgerpower.com'
+    });
+    t.is( result.id, 1 );
+    t.is( result.changes, 1 );
+  }
+)
+
+test.serial(
+  'table select',
+  async t => {
+    const users = await db.table('users');
+    const rows  = await users.select({
+      email: 'bobby@badgerpower.com'
+    });
+    t.is( rows.length, 1 );
+    t.is( rows[0].name, 'Bobby Badger' );
+  }
+)
+
+test.serial(
+  'table select columns',
+  async t => {
+    const users = await db.table('users');
+    const rows  = await users.select(
+      {
+        email: 'bobby@badgerpower.com'
+      },
+      {
+        columns: 'id name'
+      }
+    );
+    t.is( rows.length, 1 );
+    t.is( rows[0].id, 1 );
+    t.is( rows[0].name, 'Bobby Badger' );
+    t.is( rows[0].email, undefined );
+  }
+)
+
+test.serial(
+  'destroy',
+  t => {
+    db.destroy();
+    t.pass();
+  }
+)
